55c20cb5be3641bd386d118adc9adff39b79e048
rickoshi <145235121+rickoshi@users.noreply.github.com>
2025-02-15 00:25:56 -0300

    Implementa uma função para atualizar a lista de amigos

--- app.js
+++ app.js
@@ -10,5 +10,16 @@ function adicionarAmigo() {
     else {
         amigos.push(nome);
         document.getElementById("amigo").value = "";
+        percorrerArray();
+    }
+}
+
+function percorrerArray() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
     }
 }
